dcd851c3730605549f3875828f22958d7895d328
abhishek-5721 <anandabhishek5721@gmail.com>
2026-08-23 20:55:37 +0530

    Added Session, AuthController/Logout action  and refined some UI messages

---
+++ OmniTradeMvc/Models/AuthResponseViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace OmniTradeMvc.Models
+{
+    public class AuthResponseViewModel
+    {
+        public string Token { get; set; } = null!;
+        public int UserId { get; set; }
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Role { get; set; } = null!;
+    }
+}
