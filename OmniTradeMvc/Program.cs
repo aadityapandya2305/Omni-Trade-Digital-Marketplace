56e63f8552b1f3b897cb2f26664634a9127363cb
Shreyas Borkar <borkarshreyas90@gmail.com>
2026-08-23 21:01:41 +0530

    Add product listing and cart view

--- OmniTradeMvc/Program.cs
+++ OmniTradeMvc/Program.cs
@@ -9,13 +9,17 @@ namespace OmniTradeMvc
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
+            builder.Services.AddHttpClient("OmniTradeApi", client =>
+            {
+                client.BaseAddress = new Uri("https://localhost:7286/");
+            });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
